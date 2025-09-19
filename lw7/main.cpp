188a6187e7b6276679011f1d40147822ba46adb4
alexei.desktop <alexei.sadovin@mail.ru>
2025-09-20 00:24:08 +0300

    Update lw7

--- lw7/main.cpp
+++ lw7/main.cpp
@@ -1,26 +1,27 @@
+#include <Arduino.h>
+
 #include <LiquidCrystal_I2C.h>
 
-#define trigPin 5
-#define echoPin 7
+#define resistorPin 0
 
+// Arduino: SDA – A4, SCL – A5
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(resistorPin, INPUT);
   lcd.init();
   lcd.backlight();
 
   lcd.setCursor(0, 0);
   lcd.print("Light: ");
-  // lcd.print("%");
+  lcd.print("%");
 
   Serial.begin(9600);
 }
 
 int value = 0;
 void loop() {
-  value = analogRead(0);
+  value = analogRead(resistorPin);
   value = map(value, 0, 1023, 0, 100);
   lcd.setCursor(7, 1);
   lcd.print(String(value) + " %");
